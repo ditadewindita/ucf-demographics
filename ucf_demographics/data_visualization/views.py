f169fa0e9c2ce83d4e146362093a461c1c7c3869
Dita Dewindita <dita.dewindita@gmail.com>
2018-05-04 21:47:56 -0400

    Added index view with backend connection

--- ucf_demographics/data_visualization/views.py
+++ ucf_demographics/data_visualization/views.py
@@ -3,6 +3,18 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+# models
+from .models import DataByCollege, EthnicityData, GenderData
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    try:
+        by_college_data = DataByCollege.objects.get(college = "Total").limit(1)
+    except DataByCollege['DoesNotExist']:
+        raise HttpResponse(status = 404)
+
+    context = {
+        'by_college_data': by_college_data,
+    }
+    return render(request, 'data_visualization/index.html', context)
