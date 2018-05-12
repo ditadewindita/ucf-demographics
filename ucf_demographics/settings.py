d2de72facfac444d516c4d9baa71cee0ea5c2f08
Dita Dewindita <dita.dewindita@gmail.com>
2018-05-12 19:29:16 -0400

    Heroku ples work

--- ucf_demographics/settings.py
+++ ucf_demographics/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'data_visualization/static')
+STATIC_URL = '/data_visualization/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'data_visualization/static'),
 )
 
 django_heroku.settings(locals())
