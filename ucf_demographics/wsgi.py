25b960c9b62ef30db8827a76d9888fca2467728c
Dita Dewindita <dita.dewindita@gmail.com>
2018-05-12 19:21:14 -0400

    Heroku fixes

--- ucf_demographics/wsgi.py
+++ ucf_demographics/wsgi.py
@@ -8,12 +8,10 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ucf_demographics.settings")
 
 application = get_wsgi_application()
-
-from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
