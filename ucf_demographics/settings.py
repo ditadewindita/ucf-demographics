f4ddb028b42e7fb3a1454f9ce8dfc2b6f28bfd3e
Dita Dewindita <dita.dewindita@gmail.com>
2018-05-12 21:27:48 -0400

    HEROKU ONE LAST PUSH TO WORK

--- ucf_demographics/settings.py
+++ ucf_demographics/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
