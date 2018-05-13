f61352884138ef90cab3bb83b5b41bf736498bb3
Dita Dewindita <dita.dewindita@gmail.com>
2018-05-12 21:26:04 -0400

    HEROKU ONE LAST PUSH TO WORK

--- ucf_demographics/settings.py
+++ ucf_demographics/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ucf_demographics.urls'
@@ -120,14 +121,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'data_visualization/static')
-STATIC_URL = '/data_visualization/static/'
-
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'data_visualization/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 django_heroku.settings(locals())
